9358812f28e84be6729f855dc8da18745cd0e6af
David Quintero <davidq@gmail.com>
2020-10-27 16:28:22 -0400

    use template engine powers

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
 app.route("/").get((req, res) => {
-  res.render("../views/pug/index");
+  res.render("../views/pug/index", { title: "Hello", message: "Please login" });
 });
 
 app.listen(process.env.PORT || 3000, () => {
