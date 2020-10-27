615dd16dadd8aecfdbabe4c63cf02dc9291900ef
David Quintero <davidq@gmail.com>
2020-10-27 17:04:05 -0400

    connect to mongodb

--- server.js
+++ server.js
@@ -9,6 +9,36 @@ const ObjectID = require("mongodb").ObjectID;
 
 const app = express();
 
+myDB(async (client) => {
+  const myDataBase = await client.db("advanced").collection("node");
+
+  // Be sure to change the title
+
+  app.route("/").get((req, res) => {
+    res.render("../views/pug/index", {
+      title: "Connected to Database",
+      message: "Please login",
+    });
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  // Be sure to add this...
+}).catch((e) => {
+  app.route("/").get((req, res) => {
+    res.render("pug", { title: e, message: "Unable to login" });
+  });
+});
+
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
@@ -26,20 +56,6 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-passport.serializeUser((user, done) => {
-  done(null, user._id);
-});
-
-passport.deserializeUser((id, done) => {
-  myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-    done(null, null);
-  });
-});
-
-app.route("/").get((req, res) => {
-  res.render("../views/pug/index", { title: "Hello", message: "Please login" });
-});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Listening on port " + process.env.PORT);
