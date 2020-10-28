df00c321699794a3a5d2966f0d5d512fdb26c3d6
David Quintero <davidq@gmail.com>
2020-10-27 21:11:22 -0400

    social auth 1

--- routes.js
+++ routes.js
@@ -8,8 +8,22 @@ module.exports = function (app, myDataBase) {
       message: "Please login",
       showLogin: true,
       showRegistration: true,
+      showSocialAuth: true,
     });
   });
+
+  app.route("/auth/github").get((req, res) => {
+    passport.authenticate("github");
+  });
+
+  app
+    .route("/auth/github/callback")
+    .get(
+      passport.authenticate("github", { failureRedirect: "/" }),
+      (req, res) => {
+        res.redirect("/profile");
+      }
+    );
 
   app
     .route("/login")
