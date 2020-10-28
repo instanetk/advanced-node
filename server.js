c5f040518a54a5471edfc10997a04a4ef91cbb3e
David Quintero <davidq@gmail.com>
2020-10-27 20:31:57 -0400

    logout route

--- server.js
+++ server.js
@@ -38,6 +38,16 @@ myDB(async (client) => {
       username: req.user.username,
     });
   });
+
+  app.route("/logout").get((req, res) => {
+    req.logout();
+    res.redirect("/");
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).type("text").send("Not Found");
+  });
+
   // Serialization and deserialization here...
   passport.serializeUser((user, done) => {
     done(null, user._id);
