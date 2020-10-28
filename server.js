b4d308f493f0eba948c69f35e31f5a37ffb43fd5
David Quintero <davidq@gmail.com>
2020-10-27 20:06:24 -0400

    auth strategies

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require("express-session");
 const passport = require("passport");
 const ObjectID = require("mongodb").ObjectID;
+const LocalStrategy = require("passport-local");
 
 const app = express();
 
@@ -32,11 +33,29 @@ myDB(async (client) => {
     });
   });
 
+  passport.use(
+    new LocalStrategy(function (username, password, done) {
+      myDataBase.findOne({ username: username }, function (err, user) {
+        console.log("User " + username + " attempted to log in.");
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false);
+        }
+        if (password !== user.password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+  );
+
   // Be sure to add this...
 }).catch((e) => {
   app.route("/").get((req, res) => {
     res.render("pug", { title: e, message: "Unable to login" });
-  });
+  }); //DB ends
 });
 
 fccTesting(app); //For FCC testing purposes
